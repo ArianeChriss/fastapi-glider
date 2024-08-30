bc0cc2e4ca19878c24cb5cd6f15789bde1a5469c
ArianeChriss <45075120+ArianeChriss@users.noreply.github.com>
2024-08-30 09:44:07 -0400

    Update PF_1direction_updated.py

--- PF_1direction_updated.py
+++ PF_1direction_updated.py
@@ -67,32 +67,47 @@ def resample_from_index(particles, weights, indexes):
 	weights.fill (1.0 / len(weights))
 	return particles, weights
 
-def predict(glider_times, initial_positions, heading_angle, currentdata, t, v_kinematic=0.33, n_samples=200, n_g_std=0.05, n_c_std=0.05, n_t_std=0.5):
+def predict(glider_times, initial_positions, heading_angle, currentdata, t, v_kinematic, n_samples, n_g_std, n_c_std, n_t_std, duration):
 	# Calculate glider's velocity components
-	v_g_north = np.sin(heading_angle)*v_kinematic
-	v_g_east = np.cos(heading_angle)*v_kinematic
+	#v_g_north = np.sin(heading_angle)*v_kinematic
+	#v_g_east = np.cos(heading_angle)*v_kinematic
+
+	print("initial position " + str(initial_positions[0]))
+	print("heading angle radians " + str(heading_angle))
+	print("heading angle degrees " + str(math.degrees(heading_angle)))
 
 	v_c_east, v_c_north = get_closest_current(initial_positions[:,0],initial_positions[:,1], np.array(currentdata["longs"]), np.array(currentdata["lats"]), glider_times, np.array(currentdata["uEast"]), np.array(currentdata["vNorth"]), currentdata["times"]) #0,0
+	#v_c_east *= 0.5				# TAKE THESE OUT LATER
+	#v_c_north *= 0.5
+	v_g_east = np.cos(heading_angle) * np.sqrt((v_kinematic ** 2) - ((v_c_east ** 2) + (v_c_north ** 2)))
+	v_g_north = np.sin(heading_angle) * np.sqrt((v_kinematic ** 2) - ((v_c_east ** 2) + (v_c_north ** 2)))
+
+	print("v_g_east " + str(v_g_east[0]))
+	print("v_g_north " + str(v_g_north[0]))
+	print("v_c_east " + str(v_c_east[0]))
+	print("v_c_north " + str(v_c_north[0]))
+
 	n_g_mean, n_c_mean = 0, 0
 
 	# Time of dive
-	t_mean = 0
+	n_t_mean = 0
 	# Add noise
 	n_c_east = np.random.normal(n_c_mean, n_c_std, n_samples)
 	n_c_north = np.random.normal(n_c_mean, n_c_std, n_samples)
 	n_g_east = np.random.normal(n_g_mean, n_g_std, n_samples)
 	n_g_north = np.random.normal(n_g_mean, n_g_std, n_samples)
 
-	n_time = np.random.normal(t_mean, n_t_std, n_samples)
+	n_time = np.random.normal(n_t_mean, n_t_std, n_samples)
 
 	# Calculate resultant velocity v = v_g + v_c for both components
 	v_east = v_g_east + v_c_east + n_c_east + n_g_east
 	v_north = v_g_north + v_c_north + n_c_north + n_g_north
+	#v_east = np.sin(heading_angle) * v_kinematic
+	#v_north = np.cos(heading_angle) * v_kinematic
+	print("v_east " + str(v_east[0]))
+	print("v_north " + str(v_north[0]))
 
-	#print("v_east " + str(v_east))
-	#print("v_north " + str(v_north))
-
-	dive_duration = (n_time + t_mean) * 3600 # in seconds
+	dive_duration = (n_time + duration) * 3600 # in seconds
 	
 	r_earth = 6378137
 	#d_north  =  (v_north * dive_duration / r_earth) * (180 / np.pi)
@@ -101,16 +116,19 @@ def predict(glider_times, initial_positions, heading_angle, currentdata, t, v_ki
 	d_north = (v_north * dive_duration) / 111320
 	d_east = (v_east * dive_duration) / (111320 * np.cos(initial_positions[:, 1]))
 
-	#print("d_east: " + str(d_east) + ", d_north: " + str(d_north))
+	print("d_east meters " + str(v_east[0] * dive_duration[0]) + ", d_north meters " + str(v_north[0] * dive_duration[0]))
+	print("d_east " + str(d_east[0]) + ", d_north " + str(d_north[0]))
 
 	# Calculate estimated positions of the glider
 	estimated_long = initial_positions[:,0] + d_east #Finds new position, converts into lattitude and longitude again, than adds to new_position list. 
 	estimated_lat = initial_positions[:,1] + d_north
 	estimated_positions = np.array([estimated_long, estimated_lat]).T
 
+	print("estimated position " + str(estimated_positions[0]))
+
 	return estimated_positions
 
-def initialize_particles(initial_position, n_samples=200):
+def initialize_particles(initial_position, n_samples):
 	'''
 	Initializes all the particles at the same location, size (1,n_samples,2)
 	2 coordinates for longitude, latitude
@@ -127,18 +145,17 @@ def increment_pos(initial_position, commanded_position, v_kinematic, duration = 
 	heading_angle = np.arctan2(diff_in_lat,diff_in_lon)
 	v_north = np.sin(heading_angle)*v_kinematic
 	v_east = np.cos(heading_angle)*v_kinematic
-	r_earth = 6378137
-	d_north  =  (v_north * duration / r_earth) * (180 / np.pi)
-	d_east =  (v_east * duration / r_earth) * (180 / np.pi) / np.cos(initial_position[1] * np.pi/180)
+	#r_earth = 6378137
+	d_north  =  (v_north * duration) / 111320
+	d_east =  (v_east * duration) / (111320 * np.cos(initial_position[1]))
 	new_position = [initial_position[0] + d_north, initial_position[1] + d_east]
 	return np.array(new_position)
 
-
-def run_filter_one(calibration, duration, filterdata, desLong, desLat, glider_times, glider_longs, glider_lats, glider_wpt_longs, glider_wpt_lats, v_kinematic=0.33, n_samples=200, n_g_std=0.05, n_c_std=0.05, n_t_std=0.05):
+def run_filter_one(calibration, duration, filterdata, desLong, desLat, glider_times, glider_longs, glider_lats, glider_wpt_longs, glider_wpt_lats, v_kinematic=0.33, n_samples=200, n_g_std=0.01, n_c_std=0.01, n_t_std=0.05, increment = 2):
 	returndata = {"actual": filterdata, "predictions": {}}
 	measured_positions = np.column_stack((np.array(glider_longs), np.array(glider_lats)))
 	initial_positions = measured_positions
-	resampled_positions = np.zeros((1,200,2))
+	resampled_positions = np.zeros((1,n_samples,2))
 
 	commanded_positions = np.column_stack((np.array(glider_wpt_longs), np.array(glider_wpt_lats)))
 	weights = np.ones(n_samples) / n_samples
@@ -146,20 +163,40 @@ def run_filter_one(calibration, duration, filterdata, desLong, desLat, glider_ti
 	estimated_positions = measured_positions
 
 	times = np.arange(0, calibration, 2)
-	print("initial " + str(estimated_positions[0]))
+	#print("initial " + str(estimated_positions[0]))
 	for t in range(1, calibration):
-		glider_pos = increment_pos(measured_positions[t - 1], commanded_positions[t - 1], v_kinematic, duration = 2) # where it SHOULD be going by dead reckoning
+		glider_pos = increment_pos(measured_positions[t - 1], commanded_positions[t - 1], v_kinematic, increment) # where it SHOULD be going by dead reckoning
 		initial_positions = initialize_particles(measured_positions[t - 1], n_samples = n_samples)
 		diff_in_lon = float(glider_wpt_longs[t - 1]) - float(measured_positions[t - 1][0])
 		diff_in_lat = float(glider_wpt_lats[t - 1]) - float(measured_positions[t - 1][1])
-		heading_angle = np.arctan2(diff_in_lat,diff_in_lon)
+		#heading_angle = np.arctan2(diff_in_lat,diff_in_lon)
+
+		# add in edge case to other functions where it's already at the waypoint (or possibly very close?)
+		# maybe switch back to haversine formula?
+		if (diff_in_lon == 0):
+			if (diff_in_lat == 0):
+				heading_angle = 0
+			elif (diff_in_lat < 0):
+				heading_angle = -90
+			else:
+				heading_angle = 90
+		elif (diff_in_lon < 0):
+			if (diff_in_lat == 0):
+				heading_angle = 180
+			else:
+				heading_angle = math.atan(diff_in_lat / diff_in_lon)
+		else:
+			if (diff_in_lat == 0):
+				heading_angle = 0
+			else:
+				heading_angle = math.atan(diff_in_lat / diff_in_lon)
 
 		landmarks = [measured_positions[t]]
 		NL = 1
 		zs = (norm(landmarks - glider_pos, axis=1) + (randn(NL) * (n_g_std + n_c_std + n_t_std)))
 
-		estimated_positions = predict(glider_times, initial_positions, heading_angle, filterdata["currents"], t - 1, v_kinematic = 0.33)
-		print("estimated " + str(estimated_positions[0]))
+		estimated_positions = predict(glider_times, initial_positions, heading_angle, filterdata["currents"], t - 1, v_kinematic, n_samples, n_g_std, n_c_std, n_t_std, duration)
+		#print("estimated " + str(estimated_positions[0]))
 		estimated_positions, weights = update(estimated_positions, weights, z=zs, R=n_g_std + n_c_std + n_t_std, landmarks=landmarks)
 
 		if neff(weights) < n_samples/2:
